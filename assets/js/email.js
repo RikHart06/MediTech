e976b0bf0e6593ca865dad2e43ceaaa47c6cc0ee
RikHart <rsrodriguez06@gmail.com>
2023-04-25 16:08:34 -0600

    Se arregla validación de form

--- assets/js/email.js
+++ assets/js/email.js
@@ -7,33 +7,31 @@ function sendMail() {
     position: $("#position").val(),
     phone: $("#phone").val(),
     mail: $("#mail").val(),
-    message: $("#message").val()
+    message: $("#message").val(),
   };
 
   const serviceId = "service_1t8qkns";
   const templateId = "template_vxolw5s";
 
-emailjs.send(serviceId,templateId,params)
-.then(
-    res =>{
-        $("#name").val("");
-        $("#surname").val("");
-        $("#subject").val("");
-        $("#empresa").val("");
-        $("#position").val("");
-        $("#phone").val("");
-        $("#mail").val("");
-        $("#message").val("");
-        console.log(res);
-        Swal.fire({
-            title: 'Éxito!',
-            text: 'El mensaje se ha enviado correctamente',
-            icon: 'success',
-            confirmButtonText: 'Cerrar'
-          });
-    }
-)
-.catch((err) => console.log(err));
+  emailjs
+    .send(serviceId, templateId, params)
+    .then((res) => {
+      $("#name").val("");
+      $("#surname").val("");
+      $("#subject").val("");
+      $("#empresa").val("");
+      $("#position").val("");
+      $("#phone").val("");
+      $("#mail").val("");
+      $("#message").val("");
+      console.log(res);
+      $("#submitBtn").prop("disabled", false);
+      Swal.fire({
+        title: "Éxito!",
+        text: "El mensaje se ha enviado correctamente",
+        icon: "success",
+        confirmButtonText: "Cerrar",
+      });
+    })
+    .catch((err) => console.log(err));
 }
-
-
